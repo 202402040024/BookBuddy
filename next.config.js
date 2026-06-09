448855a0480dd403296adab7061fdc40a45ccde5
202402040024 <202402040024@mitaoe.ac.in>
2026-06-09 11:25:25 +0530

    Update BookBuddy project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Allow NextAuth to auto-detect the URL on Vercel
+  // No need to hardcode NEXTAUTH_URL when VERCEL_URL is available
+};
 
 module.exports = nextConfig;
